115fbe24868704942cf6647e67c138654aa73ae3
Federico Di Pierro <nierro92@gmail.com>
2014-07-01 10:19:47 +0200

    optimized largest prime factor finder

--- largest_prime_factor.c
+++ largest_prime_factor.c
@@ -8,11 +8,18 @@ int isPrime(long x);
 int main (void)
 {
 	long i;
-	for (i = 3; i < N / 2; i = i + 2) {
-		if ((N % i == 0) && isPrime(N / i))
-			break;
+	long largest = 0;
+	// Considero solo i numeri dispari (nessun pari sarà mai divisore del numero)
+	for (i = 3; i < sqrt(N); i = i + 2) {
+		// se i è divisore, e N / i è primo, N/i è il numero che stiamo cercando.
+		if (N % i == 0) {
+			if (isPrime(N / i) && N / i > largest)
+				largest = N / i;
+			else if (isPrime(i) && i > largest)
+				largest = i;
+		}
 	}
-	printf("%lu", N / i);
+	printf("%lu", largest);
 	return 0;
 }
 
