cbc0bdab8173db38b288d7ed8246d6741aaea5a9
Federico Di Pierro <nierro92@gmail.com>
2015-05-22 13:00:08 +0200

    unified case 1000 and case 100

--- num_to_string.c
+++ num_to_string.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX 99999
+
 const char lower_str[29][20] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                     "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 const char dozens_str[8][20] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
@@ -15,18 +17,15 @@ int main(void)
     do {
         printf("Insert number:> ");
         scanf("%d", &x);
-    } while (x > 99999);
+    } while (x > MAX);
     while (i != 0) {
         if (x / i != 0) {
             switch (i) {
-            case 1000:
-                if ((x / i < 20) || ((x / i) % 10 == 0))
-                    sprintf(temp, "%s %s ", lower_str[x / i - 1], high_str[1]);
+            case 1000: case 100:
+                if ((i == 100) || (x / i < 20) || ((x / i) % 10 == 0))
+                    sprintf(temp, "%s %s ", lower_str[x / i - 1], high_str[i / 1000]);
                 else
                     sprintf(temp, "%s%s %s ", dozens_str[(x / (i * 10))  - 2], lower_str[(x % (i * 10) / 1000) - 1] , high_str[1]);
-                break;
-            case 100:
-                sprintf(temp, "%s %s ", lower_str[x / i - 1], high_str[0]);
                 break;
             case 10: case 1:
                 if (x < 20) {
