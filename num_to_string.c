479884c7f3624f42b503a5e110d6b7858739ee49
Federico Di Pierro <nierro92@gmail.com>
2015-05-22 13:52:21 +0200

    final version :D

--- num_to_string.c
+++ num_to_string.c
@@ -6,13 +6,13 @@
 const char lower_str[19][20] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                     "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 const char dozens_str[8][20] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-const char high_str[2][20] = {"hundred", "thousand"};
+const char high_str[2][20] = {"hundred ", "thousand "};
 
 int main(void)
 {
     char final_str[100];
     char temp[20];
-    int x, i = 1000;
+    int x, i = 100000;
     final_str[0] = '\0';
     do {
         printf("Insert number:> ");
@@ -21,32 +21,23 @@ int main(void)
     while (i != 0) {
         if (x / i != 0) {
             switch (i) {
-                case 1000: case 100:
-                if ((i == 100) || (x / i < 20) || ((x / i) % 10 == 0))
-                    sprintf(temp, "%s %s ", lower_str[x / i - 1], high_str[i / 1000]);
-                else {
-                    if ((x / i) >= 100) {
-                        sprintf(final_str, "%s %s ", lower_str[x / (i * 100) - 1], high_str[0]);
-                        x = x % (i * 100);
-                    }
-                    sprintf(temp, "%s%s %s ", dozens_str[(x / (i * 10))  - 2], lower_str[(x % (i * 10) / 1000) - 1] , high_str[1]);
-                }
+            case 100: case 100000:
+                sprintf(temp, "%s %s", lower_str[x / i - 1], high_str[0]);
                 break;
-            case 10: case 1:
-                if (x < 20) {
-                    sprintf(temp, "%s", lower_str[x - 1]);
-                    i = 0;
-                } else {
+            case 10: case 10000:
+                if (x > 20)
                     sprintf(temp, "%s", dozens_str[(x / i) - 2]);
-                }
+                break;
+            case 1: case 1000:
+                sprintf(temp, "%s ", lower_str[x / i - 1]);
                 break;
             }
             strcat(final_str, temp);
+            if (i == 1000)
+                strcat(final_str, high_str[1]);
         }
-        if (i != 0) {
-            x = x % i;
-            i = i / 10;
-        }
+        x = x % i;
+        i = i / 10;
     }
     printf("%s\n", final_str);
     return 0;
