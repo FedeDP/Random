f06449f99784340ffad82df199c52a2675c20bcf
Federico Di Pierro <nierro92@gmail.com>
2015-05-22 15:14:47 +0200

    print directly without storing

--- num_to_string.c
+++ num_to_string.c
@@ -11,33 +11,30 @@ const char high_str[2][20] = {"hundred ", "thousand "};
 
 int main(void)
 {
-    char final_str[100], temp[20];
     int x, i = 100000;
-    final_str[0] = '\0';
     do {
         printf("Insert number:> ");
         scanf("%d", &x);
     } while (x > MAX);
     while (i != 0) {
         if (x / i != 0) {
-            memset(temp, 0, strlen(temp));
             switch (i) {
             case 10: case 10000:
-                if (x / i > 1)
-                    sprintf(temp, "%s", dozens_str[(x / i) - 2]);
+                if (x / i > 1) {
+                    printf("%s", dozens_str[(x / i) - 2]);
+                    x = x % i;
+                }
                 break;
             default:
-                sprintf(temp, "%s ", lower_str[x / i - 1]);
+                printf("%s ", lower_str[x / i - 1]);
+                x = x % i;
                 break;
             }
-            strcat(final_str, temp);
             if ((i == 100) || (i == 100000) || (i == 1000))
-                strcat(final_str, high_str[(i / 1000) % 100]);
+                printf("%s", high_str[(i / 1000) % 100]);
         }
-        if (strlen(temp))
-            x = x % i;
         i = i / 10;
     }
-    printf("%s\n", final_str);
+    printf("\n");
     return 0;
 }
