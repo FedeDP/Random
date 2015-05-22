912415150d5957f635b96aff136c920dc268e750
Federico Di Pierro <nierro92@gmail.com>
2015-05-22 18:00:13 +0200

    too funny :D

--- num_to_string.c
+++ num_to_string.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 #define MAX 999999
 
@@ -7,15 +8,16 @@ const char lower_str[19][20] = {"one", "two", "three", "four", "five", "six", "s
                                 "eight", "nine", "ten", "eleven", "twelve", "thirteen",
                                 "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
 const char dozens_str[8][20] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-const char high_str[2][20] = {"hundred ", "thousand "};
+const char higher_str[2][20] = {"hundred ", "thousand "};
 
 int main(void)
 {
-    int x, i = 100000;
+    int x, i;
     do {
         printf("Insert number:> ");
         scanf("%d", &x);
     } while (x > MAX);
+    i = pow(10, (int) log10(x));
     while (i != 0) {
         if (x / i != 0) {
             switch (i) {
@@ -26,12 +28,12 @@ int main(void)
                 }
                 break;
             default:
-                printf("%s ", lower_str[x / i - 1]);
+                printf("%s ", lower_str[(x / i) - 1]);
                 x = x % i;
                 break;
             }
             if ((i == 100) || (i == 100000) || (i == 1000))
-                printf("%s", high_str[(i / 1000) % 100]);
+                printf("%s", higher_str[(i / 1000) % 100]);
         }
         i = i / 10;
     }
