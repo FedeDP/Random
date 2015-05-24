68ff91e645012b4f1cc32df3ab457d0a36f2fac7
Federico Di Pierro <nierro92@gmail.com>
2015-05-24 12:52:01 +0200

    and now you must pass directly 1 or more number to the program command, ie: ./nts 45 67 89244

--- num_to_string.c
+++ num_to_string.c
@@ -13,21 +13,29 @@ const char higher_str[2][20] = {"hundred ", "thousand "};
 
 int main(int argc, char *argv[])
 {
-    int x, i, n;
-    for (x = atoi(argv[1]), n = 0, i = pow(10, (int) log10(x)); i != 0; x %= i, i /= 10, n = 0) {
-        if (x / i != 0) {
-            if ((i == 10) || (i == 10000)) {
-                if (x / i != 1)
-                    n += 18;
-                else
-                    i /= 10;
+    int x, i, n, p = 1;
+    if (argc < 2) {
+        printf("Usage: nts $num $num2 $num3...\n");
+        return 1;
+    }
+    for (p = 1; argv[p]; p++, printf("\n")) {
+        x = atoi(argv[p]);
+        if (x > MAX)
+            continue;
+        for (n = 0, i = pow(10, (int) log10(x)); i != 0; x %= i, i /= 10, n = 0) {
+            if (x / i != 0) {
+                if ((i == 10) || (i == 10000)) {
+                    if (x / i != 1)
+                        n += 18;
+                    else
+                        i /= 10;
+                }
+                n += (x / i) - 1;
+                printf("%s", lower_str[n]);
+                if ((i == 100) || (i == 100000) || (i == 1000))
+                    printf(" %s", higher_str[(i / 1000) % 100]);
             }
-            n += (x / i) - 1;
-            printf("%s", lower_str[n]);
-            if ((i == 100) || (i == 100000) || (i == 1000))
-                printf(" %s", higher_str[(i / 1000) % 100]);
         }
     }
-    printf("\n");
     return 0;
 }
