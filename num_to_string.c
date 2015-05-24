97f7d6f5255773358b3ffe4ecaef33faecf538d4
Federico Di Pierro <nierro92@gmail.com>
2015-05-24 12:43:13 +0200

    even shorter!

--- num_to_string.c
+++ num_to_string.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define MAX 999999
 
-const char lower_str[19][20] = {"one", "two", "three", "four", "five", "six", "seven",
+const char lower_str[27][20] = {"one", "two", "three", "four", "five", "six", "seven",
                                 "eight", "nine", "ten", "eleven", "twelve", "thirteen",
-                                "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-const char dozens_str[8][20] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+                                "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+                                "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 const char higher_str[2][20] = {"hundred ", "thousand "};
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    int x, i;
-    do {
-        printf("Insert number:> ");
-        scanf("%d", &x);
-    } while (x > MAX);
-    i = pow(10, (int) log10(x));
-    while (i != 0) {
+    int x, i, n;
+    for (x = atoi(argv[1]), n = 0, i = pow(10, (int) log10(x)); i != 0; x %= i, i /= 10, n = 0) {
         if (x / i != 0) {
-            switch (i) {
-            case 10: case 10000:
-                if (x / i > 1) {
-                    printf("%s", dozens_str[(x / i) - 2]);
-                    x = x % i;
-                }
-                break;
-            default:
-                printf("%s ", lower_str[(x / i) - 1]);
-                x = x % i;
-                break;
+            if ((i == 10) || (i == 10000)) {
+                if (x / i != 1)
+                    n += 18;
+                else
+                    i /= 10;
             }
+            n += (x / i) - 1;
+            printf("%s", lower_str[n]);
             if ((i == 100) || (i == 100000) || (i == 1000))
-                printf("%s", higher_str[(i / 1000) % 100]);
+                printf(" %s", higher_str[(i / 1000) % 100]);
         }
-        i = i / 10;
     }
     printf("\n");
     return 0;
