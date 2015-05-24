3c478c8764ab6b577873f5e556291a2f198f275c
Federico Di Pierro <nierro92@gmail.com>
2015-05-24 14:54:52 +0200

    removed other magic numbers

--- num_to_string.c
+++ num_to_string.c
@@ -9,11 +9,11 @@ const char lower_str[27][20] = {"one", "two", "three", "four", "five", "six", "s
                                 "eight", "nine", "ten", "eleven", "twelve", "thirteen",
                                 "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
                                 "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-const char higher_str[2][20] = {"hundred ", "thousand "};
+const char higher_str[2][20] = {"hundred", "thousand"};
 
 int main(int argc, char *argv[])
 {
-    int x, i, n, p = 1;
+    int x, i, n, p = 1, l;
     if (argc < 2) {
         printf("Usage: nts $num $num2 $num3...\n");
         return 1;
@@ -22,9 +22,9 @@ int main(int argc, char *argv[])
         x = atoi(argv[p]);
         if (x > MAX)
             continue;
-        for (n = 0, i = pow(10, (int) log10(x)); i != 0; x %= i, i /= 10, n = 0) {
+        for (n = 0, i = pow(10, (int)log10(x)); i != 0; x %= i, i /= 10, n = 0) {
             if (x / i != 0) {
-                if ((int)(log10(i / 10)) % 3 == 0) {
+                if ((x != 1) && ((int)(log10(i / 10)) % 3 == 0)) {
                     if (x / i != 1)
                         n += 18;
                     else
@@ -32,8 +32,9 @@ int main(int argc, char *argv[])
                 }
                 n += (x / i) - 1;
                 printf("%s", lower_str[n]);
-                if ((i == 100) || (i == 100000) || (i == 1000))
-                    printf(" %s", higher_str[(i / 1000) % 100]);
+                l = (int)(log10(i)) % 3;
+                if ((i != 1) && ((l == 0) || (l == 2)))
+                    printf(" %s ", higher_str[((int)log10(i) + 1) % 3]);
             }
         }
     }
