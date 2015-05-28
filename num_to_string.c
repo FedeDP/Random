487d1e44579e4f8dbe29ce94d05df078132249cb
Federico Di Pierro <nierro92@gmail.com>
2015-05-28 10:07:24 +0200

    new limit: unsigned int max->4 294 967 295

--- num_to_string.c
+++ num_to_string.c
@@ -2,25 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
-
-#define MAX 999999
+#include <limits.h> // max number will be 4 294 967 295
 
 const char lower_str[27][20] = {"one", "two", "three", "four", "five", "six", "seven",
                                 "eight", "nine", "ten", "eleven", "twelve", "thirteen",
                                 "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
                                 "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-const char higher_str[2][20] = {"hundred", "thousand"};
+const char higher_str[4][20] = {"hundred", "thousand", "million", "billion"};
 
 int main(int argc, char *argv[])
 {
-    int x, i, n, p = 1, l;
+    int i, n, p = 1, l;
+    unsigned int x;
     if (argc < 2) {
         printf("Usage: nts $num $num2 $num3...\n");
         return 1;
     }
     for (p = 1; argv[p]; p++, printf("\n")) {
         x = atoi(argv[p]);
-        if (x > MAX)
+        if (x > UINT_MAX)
             continue;
         for (n = 0, i = pow(10, (int)log10(x)); i != 0; x %= i, i /= 10, n = 0) {
             if (x / i != 0) {
@@ -33,8 +33,13 @@ int main(int argc, char *argv[])
                 n += (x / i) - 1;
                 printf("%s", lower_str[n]);
                 l = (int)(log10(i)) % 3;
-                if ((i != 1) && ((l == 0) || (l == 2)))
-                    printf(" %s ", higher_str[(l + 1) % 3]);
+                if ((i != 1) && ((l == 0) || (l == 2))) {
+                    if (l == 2)
+                        l = 0;
+                    else
+                        l = (int)(log10(i)) / 3;
+                    printf(" %s ", higher_str[l]);
+                }
             }
         }
     }
